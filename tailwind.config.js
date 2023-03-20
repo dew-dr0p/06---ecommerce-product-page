75ed65d32e1b4e3d77714070fe8d25edc045af8c
Wisdom-Daniel Efe <efewisdomd01@gmail.com>
2023-03-20 08:28:53 +0100

    Finalized styles for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
+    colors: {
+      white: '#FFFFFF',
+      black: '#000000',
+      orange: {
+        DEFAULT: '#FF7D1A',
+        pale: '#FFEDE0'
+      },
+      blue: {
+        veryDark: '#1D2025',
+        grayish: {
+          DEFAULT: '#B6BCC8',
+          dark: '#68707D',
+          light: '#F7F8FD'
+        }
+      }
+    },
     extend: {},
   },
   plugins: [],
