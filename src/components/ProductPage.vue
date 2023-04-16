7643f2bc7b405b11e41fac86b67ad5a5abda2080
Wisdom-Daniel Efe <efewisdomd01@gmail.com>
2023-04-16 20:07:22 +0100

    Worked on Product Popup functionality

--- src/components/ProductPage.vue
+++ src/components/ProductPage.vue
@@ -2,10 +2,12 @@
 import { ref } from 'vue'
 
 window.onload = () => {
+    console.log(document.getElementsByTagName('object')[0].contentDocument?.getElementsByClassName('white')[0].getAttribute('fill'))
     const object = document.getElementsByTagName('object')[0]
     const white = object.contentDocument?.getElementsByClassName('white')[0]
     white?.setAttribute('fill', 'white')
     white?.getAttribute('hover:width')
+    console.log(document.getElementsByTagName('object')[0].contentDocument?.getElementsByClassName('white')[0].getAttribute('fill'))
 }
 
 const products = {
@@ -23,10 +25,35 @@ const products = {
     ]
 }
 
+const index = ref(0)
+
+const showProduct = (thumbnail: string) => {
+    index.value = products.thumbnail.indexOf(thumbnail)
+}
+
+const nextProduct = () => {
+    index.value += 1
+    index.value > 3 ? index.value = 0 : ''
+}
+
+const prevProduct = () => {
+    index.value -= 1
+    index.value < 0 ? index.value = 3 : ''
+}
+
 const popUp = ref(false)
 
-const openPopUp = () => {
+const openPopUp = (thumbnail: string) => {
     popUp.value = true
+
+    // document.getElementById('close')?.ownerDocument.getElementsByClassName('white')[0].setAttribute('fill', 'white')
+    console.log(document.getElementsByTagName('object'))
+    console.log(document.getElementsByTagName('object').length)
+    // console.log(document.getElementsByTagName('object')[0].contentDocument?.getElementsByClassName('white')[0].getAttribute('fill'))
+    document.getElementsByTagName('object')[0].contentDocument?.getElementsByClassName('white')[0].setAttribute('fill', 'white')
+    // console.log(document.getElementsByTagName('object')[0].contentDocument?.getElementsByClassName('white')[0].getAttribute('fill'))
+    
+    showProduct(thumbnail)
 }
 
 const closePopUp = () => {
@@ -39,19 +66,22 @@ const closePopUp = () => {
         <img src="/assets/image-product-1.jpg" alt="" class="rounded-3xl mb-8 max-w-md">
         <div class="flex flex-row justify-between">
             <div v-for="thumbnail in products.thumbnail" class="rounded-2xl" :class="thumbnail === '/assets/image-product-1-thumbnail.jpg' ? 'border-[3px] border-orange' : ''">
-                <img :src="thumbnail" alt="" class="rounded-xl max-w-[6.25rem] cursor-pointer" :class="thumbnail === '/assets/image-product-1-thumbnail.jpg' ? 'opacity-25' : ''" @click="openPopUp">
+                <img :src="thumbnail" alt="" class="rounded-xl max-w-[6.25rem] cursor-pointer hover:opacity-50" :class="thumbnail === '/assets/image-product-1-thumbnail.jpg' ? 'opacity-25' : ''" @click="openPopUp(thumbnail)">
             </div>
         </div>
     </div>
-    <div v-if="popUp" class="absolute z-50 bg-black bg-opacity-75 w-screen h-screen overflow-hidden">
-        <div class="m-auto w-fit h-fit grid">
-            <img src="/assets/icon-close.svg" alt="" class="w-6 justify-self-end m-5 mr-0" @click="closePopUp">
-            <img src="/assets/icon-previous.svg" alt="" class="absolute top-[45%] -left-8 bg-white rounded-[50%] w-16 h-16 px-6 py-5 z-[51]">
-            <img src="/assets/icon-next.svg" alt="" class="absolute top-[45%] -right-8 bg-white rounded-[50%] w-16 h-16 px-6 py-5 z-[51]">
-            <img src="/assets/image-product-1.jpg" alt="" class="rounded-3xl mb-8 max-w-xl">
+    <div v-if="popUp" class="absolute z-40 -top-[27%] bg-black bg-opacity-75 w-screen h-screen" @click="closePopUp"></div>
+    <div v-if="popUp" class="fixed z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-fit h-fit flex">
+        <div class="grid">
+            <!-- <button class="justify-self-end m-5 mr-0 cursor-pointer" @click="closePopUp"><object id="close" data="/assets/icon-close.svg" type="image/svg+xml" title="Icon Close" class="scale-150"></object></button> -->
+            <object id="close" data="/assets/icon-close.svg" type="image/svg+xml" title="Icon Close" class="scale-150 justify-self-end m-5 mr-0 cursor-pointer" @click="closePopUp"></object>
+            <img src="/assets/icon-previous.svg" alt="" class="absolute top-[45%] -left-8 bg-white rounded-[50%] w-16 h-16 px-6 py-5 z-[51]" @click="prevProduct">
+            <img src="/assets/icon-next.svg" alt="" class="absolute top-[45%] -right-8 bg-white rounded-[50%] w-16 h-16 px-6 py-5 z-[51]" @click="nextProduct">
+            <!-- <img src="/assets/image-product-1.jpg" alt="" class="rounded-3xl mb-8 max-w-xl"> -->
+            <img :src="products.main[index]" alt="" class="rounded-3xl mb-8 max-w-xl">
             <div class="flex flex-row mx-12 justify-between">
-                <div v-for="thumbnail in products.thumbnail" class="rounded-2xl bg-white" :class="thumbnail === '/assets/image-product-1-thumbnail.jpg' ? 'border-[3px] border-orange' : ''">
-                    <img :src="thumbnail" alt="" class="rounded-xl max-w-[6.25rem] cursor-pointer" :class="thumbnail === '/assets/image-product-1-thumbnail.jpg' ? 'opacity-25' : ''">
+                <div v-for="thumbnail in products.thumbnail" class="rounded-2xl bg-white" :class="products.thumbnail.indexOf(thumbnail) === index ? 'border-[3px] border-orange' : ''">
+                    <img :src="thumbnail" alt="" class="rounded-xl max-w-[6.25rem] cursor-pointer hover:opacity-50" :class="products.thumbnail.indexOf(thumbnail) === index ? 'opacity-25' : ''" @click="showProduct(thumbnail)">
                 </div>
             </div>
         </div>
@@ -71,11 +101,7 @@ const closePopUp = () => {
             </div>
             <div></div>
             <div class="flex flex-row px-10 py-3 bg-orange text-white w-full text-3xl font-bold items-center justify-center rounded-lg shadow-lg shadow-orange">
-                <!-- <img src="/assets/icon-cart.svg" alt="" class=""> -->
                 <object data="/assets/icon-cart.svg" type="image/svg+xml" title="Icon Cart" class="pr-5"></object>
-                <!-- <svg>
-                    <use xlink-href="/assets/icon-cart.svg"></use>
-                </svg> -->
                 <p class="text-xl font-bold">Add to cart</p>
             </div>
         </div>
